da930439773e16501a29153e91754e423d680d7f
Alexey Spiridonov <92844309+SpirikStar@users.noreply.github.com>
2025-09-23 12:21:40 +0300

    update

---
+++ CourseFlow/appModels/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from .models import Course, Topic
+
+
+class TopicInline(admin.StackedInline):
+    model = Topic
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    inlines = [TopicInline]
+    list_display = ['title', 'status', 'date_created']
+    list_display_links = ['title', 'status']
+    list_filter = ['status', 'date_created']
+    search_fields = ['title', 'description']
+
+    list_per_page = 20
+
+
+@admin.register(Topic)
+class TopicAdmin(admin.ModelAdmin):
+    autocomplete_fields = ['course']
---
+++ CourseFlow/appModels/models.py
@@ -0,0 +1,56 @@
+from django.db import models
+
+
+class Course(models.Model):
+    title = models.CharField(
+        verbose_name='Название'
+    )
+    description = models.TextField(
+        verbose_name='Описание', 
+        null=True, blank=True
+    )
+    status = models.BooleanField(
+        verbose_name='Статус', 
+        choices=[
+            (True, 'Опубликован'),
+            (False, 'Не опубликован'),
+        ],
+        default=True
+    )
+    date_created = models.DateField(
+        verbose_name='Дата создания', 
+        auto_now_add=True
+    )
+
+    def __str__(self):
+        return self.title
+    class Meta:
+        verbose_name = 'курс'
+        verbose_name_plural = 'Курсы'
+
+
+class Topic(models.Model):
+    course = models.ForeignKey(
+        Course,
+        on_delete=models.CASCADE,
+        verbose_name='Курс'
+    )
+    title = models.CharField(
+        verbose_name='Название'
+    )
+    description = models.TextField(
+        verbose_name='Описание', 
+        null=True, blank=True
+    )
+    status = models.BooleanField(
+        verbose_name='Статус', 
+        choices=[
+            (True, 'Опубликован'),
+            (False, 'Не опубликован'),
+        ],
+        default=True
+    )
+
+    class Meta:
+        verbose_name = 'тему'
+        verbose_name_plural = 'Темы'
