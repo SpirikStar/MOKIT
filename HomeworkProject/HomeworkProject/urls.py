0a3fb0c22fb73afc57bed4bee57dc00209130ac7
Alexey Spiridonov <92844309+SpirikStar@users.noreply.github.com>
2025-10-07 14:40:13 +0300

    update

--- HomeworkProject/HomeworkProject/urls.py
+++ HomeworkProject/HomeworkProject/urls.py
@@ -1,7 +1,10 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from . import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.HomeView.as_view()),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
