cf25252f3d0c8dc102d078c78d9037e5df2202f6
Alexey Spiridonov <92844309+SpirikStar@users.noreply.github.com>
2025-10-27 12:42:37 +0300

    update

--- appRequest/views.py
+++ appRequest/views.py
@@ -1,6 +1,7 @@
 from appModels import models
 from django.views import View
 from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
 
 class PersonalAccountPage(View):
     def get(self, request):
@@ -13,6 +14,25 @@ class AuthPage(View):
         if request.user.is_authenticated:
             return redirect('/')
         return render(request, 'auth/index.html')
+    
+    def post(self, request):
+        username = request.POST.get('us_login')
+        password = request.POST.get('us_password')
+
+        user = authenticate(request, username=username, password=password)
+        if not user:
+            return redirect('/auth/?error=Неверный логин или пароль')
+        
+        if not user.is_active:
+            return redirect('/auth/?error=Пользователь заблокирован')
+        
+        login(request, user)
+
+        if user.is_staff:
+            return redirect('/admin/')
+        
+        return redirect('/')
+
 
 class RegPage(View):
     def get(self, request):
