8e4b7c5ac68a27fb63225d9998adae1b4d21c3ec
Alexey Spiridonov <92844309+SpirikStar@users.noreply.github.com>
2025-10-27 12:07:51 +0300

    update

--- appRequest/views.py
+++ appRequest/views.py
@@ -1,13 +1,17 @@
 from appModels import models
 from django.views import View
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 class PersonalAccountPage(View):
     def get(self, request):
+        if not request.user.is_authenticated:
+            return redirect('/auth/')
         return render(request, 'account/index.html')
 
 class AuthPage(View):
     def get(self, request):
+        if request.user.is_authenticated:
+            return redirect('/')
         return render(request, 'auth/index.html')
 
 class RegPage(View):
