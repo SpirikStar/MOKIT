905d96ff683ee47f6c21eaaa28b12059c0948d4c
Alexey Spiridonov <92844309+SpirikStar@users.noreply.github.com>
2025-10-27 09:52:06 +0300

    update

--- appModels/models.py
+++ appModels/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    phone = models.CharField(
+        verbose_name="Номер телефона"
+    )
+    middle_name = models.CharField(
+        verbose_name="Отчество",
+        max_length=120,
+        null=True,
+        blank=True
+    )
+
+    class Meta:
+        db_table = "users"
+        verbose_name = "пользователя"
+        verbose_name_plural = "Пользователи"
