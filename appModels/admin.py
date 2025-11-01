89f47bb41da5392d7041f658d05fda6b61ad02be
Alexey Spiridonov <92844309+SpirikStar@users.noreply.github.com>
2025-11-01 10:48:15 +0300

    new

--- appModels/admin.py
+++ appModels/admin.py
@@ -1,8 +1,28 @@
 from django.contrib import admin
-from appModels.models import User
+from appModels.models import User, Service, Order
 
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ['first_name', 'last_name']
     list_display_links = ['first_name', 'last_name']
+
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    # list_display - отображаемые поля
+    list_display = ['title']
+
+    # list_display_links - ссылки, которые открываются при нажатии
+    list_display_links = ['title']
+
+# admin.register() - регистрируем модель в админке
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['user', 'address', 'dtime', 'service', 'method_pay']
+    list_display_links = ['user', 'address', 'dtime', 'service', 'method_pay']
+
+    # list_filter - фильтры
+    list_filter = ['dtime', 'service', 'method_pay']
